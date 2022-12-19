9fe2a397c22ccad0ea2fce7395a53b37a6448c01
Simo Puustinen <puustinen.simo@gmail.com>
2022-12-19 20:48:17 +0200

    erased duplicate code

--- srcs/keyboard/insertion.c
+++ srcs/keyboard/insertion.c
@@ -6,7 +6,7 @@
 /*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 15:40:13 by spuustin          #+#    #+#             */
-/*   Updated: 2022/12/19 20:44:53 by spuustin         ###   ########.fr       */
+/*   Updated: 2022/12/19 20:47:55 by spuustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -289,54 +289,6 @@ static void	ft_insertion_char(t_term *t)
 		ft_quote_flag_check(t, t->index - 1);
 }
 
-static void	ft_print_line(t_term *t, ssize_t row)
-{
-	if (row == t->c_row)
-	{
-		if (t->nl_addr[row + 1])
-			write(1, &t->inp[t->index], \
-				(size_t)(t->nl_addr[row + 1] - &t->inp[t->index]));
-		else
-			write(1, &t->inp[t->index], \
-				(size_t)((&t->inp[t->bytes] - &t->inp[t->index]) + 1));
-	}
-	else
-	{
-		if (ft_is_prompt_line(t, row))
-			print_prompt(row);
-		if (t->nl_addr[row + 1])
-			write(1, t->nl_addr[row], \
-				(size_t)(t->nl_addr[row + 1] - t->nl_addr[row]));
-		else
-			write(1, t->nl_addr[row], \
-				(size_t)((&t->inp[t->bytes] - t->nl_addr[row]) + 1));
-	}
-}
-
-static void	ft_print_line_traill(t_term *t)
-{
-	ssize_t	row;
-	ssize_t	dis_row;
-
-	row = t->c_row;
-	dis_row = get_linenbr();
-	while (row <= t->total_row)
-	{
-		run_capability("ce");
-		ft_print_line(t, row++);
-		set_cursor(0, ++dis_row);
-	}
-}
-
-static void	ft_print_traill(t_term *t)
-{
-	run_capability("vi");
-	run_capability("sc");
-	ft_print_line_traill(t);
-	run_capability("rc");
-	run_capability("ve");
-}
-
 void	insertion(t_term *t)
 {
 	if (t->ch == ENTER)
@@ -345,5 +297,5 @@ void	insertion(t_term *t)
 		ft_insertion_char(t);
 	trigger_nl(t);
 	if (t->inp[t->index])
-		ft_print_traill(t);
+		ft_print_trail(t);
 }
