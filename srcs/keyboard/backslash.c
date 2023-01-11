ca65e02679f1441f7cfdf975243cffdcb051cef1
Simo Puustinen <puustinen.simo@gmail.com>
2023-01-11 20:22:20 +0200

    removed unneeded function

--- srcs/keyboard/backslash.c
+++ srcs/keyboard/backslash.c
@@ -6,30 +6,11 @@
 /*   By: spuustin <spuustin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/02 19:39:53 by spuustin          #+#    #+#             */
-/*   Updated: 2023/01/02 19:42:03 by spuustin         ###   ########.fr       */
+/*   Updated: 2023/01/11 20:21:51 by spuustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_21sh.h"
-
-void	ft_bslash_handling(t_term *t)
-{
-	ssize_t	i;
-
-	i = t->index - 1;
-	if (t->ch == '\\' && t->index == t->bytes && !t->heredoc)
-	{
-		while (i && t->inp[i] == '\\')
-			i--;
-		if ((t->index - i) % 2)
-			t->bslash = 1;
-		else
-			t->bslash = 0;
-	}
-	else
-		if (t->ch != D_QUO && t->ch != S_QUO)
-			t->bslash = 0;
-}
 
 int	ft_bslash_escape_check(t_term *t, ssize_t pos)
 {
